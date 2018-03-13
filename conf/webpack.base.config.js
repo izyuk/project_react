e4ec6a636ab2d054a4310e8f0129dd92f194c018
Izyuk <dmitriy.izyuk@intentsg.com>
2018-03-13 13:52:09 +0200

    site

---
+++ conf/webpack.base.config.js
@@ -0,0 +1,38 @@
+import Config from 'webpack-config';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+
+export default new Config().merge({
+  entry: './src/index.js',
+  output: {
+    path: __dirname + '/public',
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      {test: /\.js$/ , loader:'babel-loader', exclude: '/node_modules/'},
+      { test: /\.jsx?$/, loader: 'babel-loader', exclude: [/node_modules/, /public/] },
+      {
+        test: /\.css$/,
+        exclude: /node_modules/,
+        loaders: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.less$/,
+        exclude: /node_modules/,
+        loaders: ['style-loader', 'css-loader!postcss-loader!less-loader'],
+      },
+      { test: /\.gif$/, loader: 'url-loader?limit=10000&mimetype=image/gif' },
+      { test: /\.jpg$/, loader: 'url-loader?limit=10000&mimetype=image/jpg' },
+      { test: /\.png$/, loader: 'url-loader?limit=10000&mimetype=image/png' },
+      { test: /\.svg/, loader: 'url-loader?limit=26000&mimetype=image/svg+xml' },
+      { test: /\.(woff|woff2|ttf|eot)/, loader: 'url-loader?limit=1' },
+      { test: /\.json$/, loader: 'json-loader' },
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+      inject: "body"
+    })
+  ]
+});
--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
+import { AppContainer } from 'react-hot-loader';
 import ReactDOM from 'react-dom';
+// import AppRouter from './routes';
 import {Provider} from 'react-redux';
 import {createStore} from 'redux';
 import allReducers from './reducers';
@@ -8,10 +10,19 @@ import registerServiceWorker from './registerServiceWorker';
 
 const store = createStore(allReducers);
 
+const render = (Component) =>
 ReactDOM.render(
   <Provider store={store}>
     <Wrapper />
   </Provider>,
   document.getElementById('general')
 );
+
+render(Wrapper);
+if (module.hot) {
+  module.hot.accept('./components/wrapper', () => {
+    require('./components/wrapper');
+    render(Wrapper);
+  });
+}
 registerServiceWorker();
